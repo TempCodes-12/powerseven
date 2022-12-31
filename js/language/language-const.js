551f16474a24632d4127684c7a6fc979d198ed38
TharLin123 <tharlinhtet97@gmail.com>
2022-12-31 22:28:32 +0800

    'language-switch-feature'

---
+++ js/language/language-const.js
@@ -0,0 +1,96 @@
+const data = {
+  menu: {
+    myanmar: {
+      home: 'မူလ​',
+      visionAndMission: 'ကတိနှင့်တာဝန်',
+      products: 'ထုတ်ကုန်​ပစ္စည်း​များ',
+      ourStory: 'ကျွန်ုပ်​တို့အကြာင်း',
+      value: 'တန်ဖိုးများ',
+      services: 'ဝန်ဆောင်မှုများ',
+      contactUs: 'ဆက်သွယ်​ရန်​'
+    },
+    english: {
+      home: 'home',
+      visionAndMission: 'vision & mission',
+      products: 'products',
+      ourStory: 'our story',
+      value: 'value',
+      services: 'services',
+      contactUs: 'contact us'
+    }
+  },
+  content: {
+    myanmar: {
+      vision: 'မြန်မာနိုင်ငံ၏ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်တွင် RC နှင့် Steel Structure ဆောက်လုပ်ရေးတို့တွင်အထွတ်အထိပ်ခေါင်းဆောင်တစ်ဦးဖြစ်လာစေရန်',
+      ourMission: {
+        line1: 'ကျွန်ုပ်တို့ ၏ ဝန်ဆောင်မှုများနှင့် ထုတ်ကုန်များမှတစ်ဆင့် ဖောက်သည်များ၏လိုအပ်ချက်များအတွက်အကောင်းဆုံး အဖြေပေးအပ်ရန် ကတိပြုပါသည်။',
+        line2: 'ကျွန်ုပ်တို့အဖွဲ့အစည်းနှင့်အစဉ်အားပေးသူ ဖောက်သည်များ၊ မိတ်ဆွေသစ်၊ မိတ်ဆွေဟောင်းများ၊လက်တွဲလုပ်ကိုင်လာကြသော မိတ်ဖက်ကုမ္ပဏီများ ပူးပေါင်း၍ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်ကိုတည်ဆောက်ရန်မှာ ကျွန်ုပ်တို့ တာဝန်တရပ်သာဖြစ်သည်။'
+      },
+      ourStory: `Power Seven Construction ကို ဖေဖော်ဝါရီလ ၂၀၁၀ ခုနှစ်တွင် Founder ဖြစ်သူ ဦးဆန်နီမှ
+        မန္တလေးမြို့တွင်စတင်တည်ထောင်ခဲ့ပါသည်။စတင်တည်ထောင်ချိန်မှစ၍ အစိုးရငှာန
+        မြောက်မြားစွာဖြင့် လက်တွဲလုပ်ကိုင်ခဲ့ရာ ယခု ၂၀၂၂
+        ခုနှစ်အထိ မန္တလေးတိုင်း၊မကွေးတိုငး၊ရှမ်းပြည်နယ် နှင့် ချင်းပြည်နယ်အစရှိသော
+        ပြည်နယ်နှင့်တိုင်းအသီးသီးတွင် စီမံကိန်း
+
+        (၁၀၀)ကျော်ခန့် ပြီးမြောက်လွှဲအပ်နိုင်ခဲ့ပြီဖြစ်ပါသည်။
+        ကုန်ထုပ်လုပ်မှု Manufacturing Company ကိုမှု ၂၀၁၇ နိုဝင်ဘာတွင် တည်ထောင်ခဲ့ပြီး 4 Angle, C-Purlin ,
+        Deck-Sheet အစရှိသည့်
+        သံပြားပုံဖော်ခြင်း (roll-farming) အားစတင်လုပ်ဆောင်ခဲ့ပါသည်။
+        တစ်လထုတ်လုပ်မှုစွမ်းရည်မှာ 1500-2000 MT ဖြစ်ပြီး ယခုအခါ မြန်မာနိုင်ငံအထက်ပိုင်းတွင်
+        ဖြန့်ဖြူးရောင်းချလျှက်ရှိသည်။
+        Power Seven Construction ကို ဖေဖော်ဝါရီလ ၂၀၁၀ ခုနှစ်တွင် Founder ဖြစ်သူ ဦးဆန်နီမှ
+        မန္တလေးမြို့တွင်စတင်တည်ထောင်ခဲ့ပါသည်။စတင်တည်ထောင်ချိန်မှစ၍ အစိုးရငှာန
+        မြောက်မြားစွာဖြင့် လက်တွဲလုပ်ကိုင်ခဲ့ရာ ယခု ၂၀၂၂
+        ခုနှစ်အထိ မန္တလေးတိုင်း၊မကွေးတိုငး၊ရှမ်းပြည်နယ် နှင့် ချင်းပြည်နယ်အစရှိသော
+        ပြည်နယ်နှင့်တိုင်းအသီးသီးတွင် စီမံကိန်း
+        (၁၀၀)ကျော်ခန့် ပြီးမြောက်လွှဲအပ်နိုင်ခဲ့ပြီဖြစ်ပါသည်။
+        ကုန်ထုပ်လုပ်မှု Manufacturing Company ကိုမှု ၂၀၁၇ နိုဝင်ဘာတွင် တည်ထောင်ခဲ့ပြီး 4 Angle, C-Purlin ,
+        Deck-Sheet အစရှိသည့်
+        သံပြားပုံဖော်ခြင်း (roll-farming) အားစတင်လုပ်ဆောင်ခဲ့ပါသည်။
+        တစ်လထုတ်လုပ်မှုစွမ်းရည်မှာ 1500-2000 MT ဖြစ်ပြီး ယခုအခါ မြန်မာနိုင်ငံအထက်ပိုင်းတွင်
+        ဖြန့်ဖြူးရောင်းချလျှက်ရှိသည်။`,
+      values: {
+        people: 'အဖွဲ့အစည်း',
+        teamwork: 'အသင်းအဖွဲ့လိုက်လုပ်ကိုင်မှု',
+        trust: '-ယုံကြည်မှု',
+        integrity: '-သမာဓိရှိမှု',
+        innovation: '-ဆန်းသစ်တီထွင်မှု',
+        sustainability: '-ရည်ရှည်တည်တံ့ခိုင်မြဲမှုအဖွဲ့အစည်း'
+      },
+      services: {
+        rcc: 'RC ဆောက်လုပ်ရေး',
+        roadWay: 'လမ်း၊တံတား ဆောက်လုပ်ရေး',
+        steelStructure: 'Steel structure ဆောက်လုပ်ရေး',
+        steelFabriction: 'Steel structure ထုတ်လုပ်၊တပ်ဆင်ရေး',
+        ppgiAndGiRollForming: 'သွပ်ပြား၊စီချန်နယ်၊Deck Sheet ထုတ်လုပ်ဖြန့်ချီရေး',
+        machineryRentalAndEarthworks: 'စက်၊ယန္တရားငှားယမ်းခြင်းနှင့် မြေသားလုပ်ငန်း'
+      }
+    },
+    english: {
+      vision: 'To be the supreme lead in Myanmar’s RC and Steel Structure construction projects, in the future.',
+      ourMission: {
+        line1: 'Committed to deliver the best solution to our customers through our services and products. ',
+        line2: 'Building long-term partnership with our customers, supplier, team while creating sustainable future.'
+      },
+      ourStory: '',
+      values: {
+        people: 'people',
+        teamwork: 'teamwork',
+        trust: 'trust',
+        integrity: 'integrity',
+        innovation: 'innovation',
+        sustainability: 'sustainability'
+      },
+      services: {
+        rcc: 'R.C.C Construction',
+        roadWay: 'Roadway Construction',
+        steelStructure: 'Steel Structure Construction',
+        steelFabriction: 'Steel Fabrication',
+        ppgiAndGiRollForming: 'PPGI/GL and GI Roll-Forming',
+        machineryRentalAndEarthworks: 'Machinery Rental and Earthworks'
+      }
+    }
+  }
+}
+
+modules.exports = data
---
+++ js/language/language-script.js
@@ -0,0 +1,164 @@
+const data = {
+  menu: {
+    myanmar: {
+      home: 'မူလ​',
+      visionAndMission: 'ကတိနှင့်တာဝန်',
+      products: 'ထုတ်ကုန်​ပစ္စည်း​များ',
+      ourStory: 'ကျွန်ုပ်​တို့အကြာင်း',
+      value: 'တန်ဖိုးများ',
+      services: 'ဝန်ဆောင်မှုများ',
+      contactUs: 'ဆက်သွယ်​ရန်​'
+    },
+    english: {
+      home: 'home',
+      visionAndMission: 'vision & mission',
+      products: 'products',
+      ourStory: 'our story',
+      value: 'value',
+      services: 'services',
+      contactUs: 'contact us'
+    }
+  },
+  content: {
+    myanmar: {
+      vision: 'အနာဂတ်',
+      visionContent: 'မြန်မာနိုင်ငံ၏ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်တွင် RC နှင့် Steel Structure ဆောက်လုပ်ရေးတို့တွင်အထွတ်အထိပ်ခေါင်းဆောင်တစ်ဦးဖြစ်လာစေရန်',
+      ourMission: {
+        line1: 'ကျွန်ုပ်တို့ ၏ ဝန်ဆောင်မှုများနှင့် ထုတ်ကုန်များမှတစ်ဆင့် ဖောက်သည်များ၏လိုအပ်ချက်များအတွက်အကောင်းဆုံး အဖြေပေးအပ်ရန် ကတိပြုပါသည်။',
+        line2: 'ကျွန်ုပ်တို့အဖွဲ့အစည်းနှင့်အစဉ်အားပေးသူ ဖောက်သည်များ၊ မိတ်ဆွေသစ်၊ မိတ်ဆွေဟောင်းများ၊လက်တွဲလုပ်ကိုင်လာကြသော မိတ်ဖက်ကုမ္ပဏီများ ပူးပေါင်း၍ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်ကိုတည်ဆောက်ရန်မှာ ကျွန်ုပ်တို့ တာဝန်တရပ်သာဖြစ်သည်။'
+      },
+      ourStory: {
+        line1: 'Power Seven Construction ကို ဖေဖော်ဝါရီလ ၂၀၁၀ ခုနှစ်တွင် Founder ဖြစ်သူ ဦးဆန်နီမှမန္တလေးမြို့တွင်စတင်တည်ထောင်ခဲ့ပါသည်။',
+        line2: `စတင်တည်ထောင်ချိန်မှစ၍ အစိုးရငှာန
+        မြောက်မြားစွာဖြင့် လက်တွဲလုပ်ကိုင်ခဲ့ရာ ယခု ၂၀၂၂`,
+        line3: ` ခုနှစ်အထိ မန္တလေးတိုင်း၊မကွေးတိုငး၊ရှမ်းပြည်နယ် နှင့် ချင်းပြည်နယ်အစရှိသော
+        ပြည်နယ်နှင့်တိုင်းအသီးသီးတွင် စီမံကိန်း`,
+
+        line4: `(၁၀၀)ကျော်ခန့် ပြီးမြောက်လွှဲအပ်နိုင်ခဲ့ပြီဖြစ်ပါသည်။`,
+        line5: `       ကုန်ထုပ်လုပ်မှု Manufacturing Company ကိုမှု ၂၀၁၇ နိုဝင်ဘာတွင် တည်ထောင်ခဲ့ပြီး`,
+        line6: `4 Angle, C-Purlin ,Deck-Sheet အစရှိသည့်သံပြားပုံဖော်ခြင်း (roll-farming) အားစတင်လုပ်ဆောင်ခဲ့ပါသည်။`,
+        line7: ` တစ်လထုတ်လုပ်မှုစွမ်းရည်မှာ 1500-2000 MT ဖြစ်ပြီး ယခုအခါ မြန်မာနိုင်ငံအထက်ပိုင်းတွင်
+        ဖြန့်ဖြူးရောင်းချလျှက်ရှိသည်။`,
+
+      },
+      values: {
+        people: 'အဖွဲ့အစည်း',
+        teamwork: 'အသင်းအဖွဲ့လိုက်လုပ်ကိုင်မှု',
+        trust: 'ယုံကြည်မှု',
+        integrity: 'သမာဓိရှိမှု',
+        innovation: 'ဆန်းသစ်တီထွင်မှု',
+        sustainability: 'ရည်ရှည်တည်တံ့ခိုင်မြဲမှုအဖွဲ့အစည်း'
+      },
+      services: {
+        rcc: 'RC ဆောက်လုပ်ရေး',
+        roadWay: 'လမ်း၊တံတား ဆောက်လုပ်ရေး',
+        steelStructure: 'Steel structure ဆောက်လုပ်ရေး',
+        steelFabriction: 'Steel structure ထုတ်လုပ်၊တပ်ဆင်ရေး',
+        ppgiAndGiRollForming: 'သွပ်ပြား၊စီချန်နယ်၊Deck Sheet ထုတ်လုပ်ဖြန့်ချီရေး',
+        machineryRentalAndEarthworks: 'စက်၊ယန္တရားငှားယမ်းခြင်းနှင့် မြေသားလုပ်ငန်း'
+      }
+    },
+    english: {
+      vision: 'vision',
+      visionContent: 'To be the supreme lead in Myanmar’s RC and Steel Structure construction projects, in the future.',
+      ourMission: {
+        line1: 'Committed to deliver the best solution to our customers through our services and products. ',
+        line2: 'Building long-term partnership with our customers, supplier, team while creating sustainable future.'
+      },
+      ourStory: {
+        line1: `Founded By U Sanni in February 2010, in Mandalay.`,
+        line2: ` Partnership with government’s`,
+        line3: ` construction projects and till now 2022 in Mandalay,Magway,Shan state and Chin states.`,
+        line4: `Founded : February 2010`,
+        line5: `Manufacturing`,
+        line6: `Established : November 2017`,
+        line7: `  Roll Forming : 4-Angle Roofing`,
+        line8: ` C- Purlin and Deck Sheet`,
+        line9: ` Capacity :2000MT/month`,
+      },
+      values: {
+        people: 'people',
+        teamwork: 'teamwork',
+        trust: 'trust',
+        integrity: 'integrity',
+        innovation: 'innovation',
+        sustainability: 'sustainability'
+      },
+      services: {
+        rcc: 'R.C.C Construction',
+        roadWay: 'Roadway Construction',
+        steelStructure: 'Steel Structure Construction',
+        steelFabriction: 'Steel Fabrication',
+        ppgiAndGiRollForming: 'PPGI/GL and GI Roll-Forming',
+        machineryRentalAndEarthworks: 'Machinery Rental and Earthworks'
+      }
+    }
+  }
+}
+
+function changeLanguage(lang) {
+  let menu = document.querySelectorAll('.nav-link');
+  let vision = document.querySelector('.vision')
+  let visionContent = document.querySelector('.vision-content')
+  let mission = document.querySelector('.mission')
+  let missionContent1 = document.querySelector('.mission-content1')
+  let missionContent2 = document.querySelector('.mission-content2')
+  let values = document.querySelector('.values')
+  let value = document.querySelectorAll('.value');
+  let services = document.querySelector('.services')
+  let service = document.querySelectorAll('.service');
+  let ourStory = document.querySelector('.our-story')
+  let ourStoryContent = document.querySelectorAll('.our-story-content');
+  if(lang === 'english'){
+    const keys = Object.keys(data.menu.english);
+    const valueKeys = Object.keys(data.content.english.values);
+    const serviceKeys = Object.keys(data.content.english.services);
+    const ourStoryKeys = Object.keys(data.content.english.ourStory);
+    for (let index = 0; index < menu.length; index++) {
+      menu[index].textContent = data.menu.english[keys[index]]
+    }
+    vision.textContent = data.content.english.vision
+    visionContent.textContent = data.content.english.visionContent
+    mission.textContent = data.menu.english.visionAndMission
+    missionContent1.textContent = data.content.english.ourMission.line1
+    missionContent2.textContent = data.content.english.ourMission.line2
+    for (let index = 0; index < value.length; index++) {
+      value[index].textContent = data.content.english.values[valueKeys[index]]
+    }
+    values.textContent = data.menu.english.value
+    for (let index = 0; index < service.length; index++) {
+      service[index].textContent = data.content.english.services[serviceKeys[index]]
+    }
+    services.textContent = data.menu.english.services
+    for (let index = 0; index < ourStoryContent.length; index++) {
+      ourStoryContent[index].textContent = data.content.english.ourStory[ourStoryKeys[index]]
+    }
+    ourStory.textContent = data.menu.english.ourStory
+  } 
+  else if (lang === 'myanmar') {
+    const keys = Object.keys(data.menu.myanmar);
+    const valueKeys = Object.keys(data.content.myanmar.values);
+    const serviceKeys = Object.keys(data.content.myanmar.services);
+    const ourStoryKeys = Object.keys(data.content.myanmar.ourStory);
+    for (let index = 0; index < menu.length; index++) {
+      menu[index].textContent = data.menu.myanmar[keys[index]]
+    }
+    vision.textContent = data.content.myanmar.vision
+    visionContent.textContent = data.content.myanmar.visionContent
+    mission.textContent = data.menu.myanmar.visionAndMission
+    missionContent1.textContent = data.content.myanmar.ourMission.line1
+    missionContent2.textContent = data.content.myanmar.ourMission.line2
+    for (let index = 0; index < value.length; index++) {
+      value[index].textContent = data.content.myanmar.values[valueKeys[index]]
+    }
+    values.textContent = data.menu.myanmar.value
+    for (let index = 0; index < service.length; index++) {
+      service[index].textContent = data.content.myanmar.services[serviceKeys[index]]
+    }
+    services.textContent = data.menu.myanmar.services
+    for (let index = 0; index < ourStoryContent.length; index++) {
+      ourStoryContent[index].textContent = data.content.myanmar.ourStory[ourStoryKeys[index]]
+    }
+    ourStory.textContent = data.menu.myanmar.ourStory
+  }
+}
