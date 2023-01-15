3769536b2ec0c53385db23459d26d406f0453ed5
TharLin123 <tharlinhtet97@gmail.com>
2023-01-15 23:05:21 +0800

    add language change feature

--- js/language/language-script.js
+++ js/language/language-script.js
@@ -1,98 +1,127 @@
 const data = {
   menu: {
-    myanmar: {
-      home: 'မူလ​',
-      visionAndMission: 'ကတိနှင့်တာဝန်',
-      products: 'ထုတ်ကုန်​ပစ္စည်း​များ',
-      ourStory: 'ကျွန်ုပ်​တို့အကြာင်း',
-      value: 'တန်ဖိုးများ',
-      services: 'ဝန်ဆောင်မှုများ',
-      contactUs: 'ဆက်သွယ်​ရန်​'
-    },
-    english: {
-      home: 'home',
-      visionAndMission: 'vision & mission',
-      products: 'products',
-      ourStory: 'our story',
-      value: 'value',
-      services: 'services',
-      contactUs: 'contact us'
-    }
+    myanmar: ['မူလ​','ကတိနှင့်တာဝန်','ထုတ်ကုန်​ပစ္စည်း​များ','ကျွန်ုပ်​တို့အကြာင်း','တန်ဖိုးများ','ဝန်ဆောင်မှုများ','ဆက်သွယ်​ရန်​'],
+    english: ['home','vision & mission','products','our story','value','services','contact us']
   },
   content: {
     myanmar: {
       vision: 'အနာဂတ်',
       visionContent: 'မြန်မာနိုင်ငံ၏ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်တွင် RC နှင့် Steel Structure ဆောက်လုပ်ရေးတို့တွင်အထွတ်အထိပ်ခေါင်းဆောင်တစ်ဦးဖြစ်လာစေရန်',
-      ourMission: {
-        line1: 'ကျွန်ုပ်တို့ ၏ ဝန်ဆောင်မှုများနှင့် ထုတ်ကုန်များမှတစ်ဆင့် ဖောက်သည်များ၏လိုအပ်ချက်များအတွက်အကောင်းဆုံး အဖြေပေးအပ်ရန် ကတိပြုပါသည်။',
-        line2: 'ကျွန်ုပ်တို့အဖွဲ့အစည်းနှင့်အစဉ်အားပေးသူ ဖောက်သည်များ၊ မိတ်ဆွေသစ်၊ မိတ်ဆွေဟောင်းများ၊လက်တွဲလုပ်ကိုင်လာကြသော မိတ်ဖက်ကုမ္ပဏီများ ပူးပေါင်း၍ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်ကိုတည်ဆောက်ရန်မှာ ကျွန်ုပ်တို့ တာဝန်တရပ်သာဖြစ်သည်။'
-      },
-      ourStory: {
-        line1: 'Power Seven Construction ကို ဖေဖော်ဝါရီလ ၂၀၁၀ ခုနှစ်တွင် Founder ဖြစ်သူ ဦးဆန်နီမှမန္တလေးမြို့တွင်စတင်တည်ထောင်ခဲ့ပါသည်။',
-        line2: `စတင်တည်ထောင်ချိန်မှစ၍ အစိုးရငှာန
+      mission: 'ကျွန်ုပ်တို့၏တာဝန်',
+      missionContent: [
+        'ကျွန်ုပ်တို့ ၏ ဝန်ဆောင်မှုများနှင့် ထုတ်ကုန်များမှတစ်ဆင့် ဖောက်သည်များ၏လိုအပ်ချက်များအတွက်အကောင်းဆုံး အဖြေပေးအပ်ရန် ကတိပြုပါသည်။',
+        'ကျွန်ုပ်တို့အဖွဲ့အစည်းနှင့်အစဉ်အားပေးသူ ဖောက်သည်များ၊ မိတ်ဆွေသစ်၊ မိတ်ဆွေဟောင်းများ၊လက်တွဲလုပ်ကိုင်လာကြသော မိတ်ဖက်ကုမ္ပဏီများ ပူးပေါင်း၍ ရေရှည်တည်တံ့ခိုင်မြဲသော အနာဂတ်ကိုတည်ဆောက်ရန်မှာ ကျွန်ုပ်တို့ တာဝန်တရပ်သာဖြစ်သည်။'
+      ],
+      ourStory: 'ကျွန်ုပ်​တို့အကြာင်း',
+      ourStoryContent: [
+        'Power Seven Construction ကို ဖေဖော်ဝါရီလ ၂၀၁၀ ခုနှစ်တွင် Founder ဖြစ်သူ ဦးဆန်နီမှမန္တလေးမြို့တွင်စတင်တည်ထောင်ခဲ့ပါသည်။',
+        `စတင်တည်ထောင်ချိန်မှစ၍ အစိုးရငှာန
         မြောက်မြားစွာဖြင့် လက်တွဲလုပ်ကိုင်ခဲ့ရာ ယခု ၂၀၂၂`,
-        line3: ` ခုနှစ်အထိ မန္တလေးတိုင်း၊မကွေးတိုငး၊ရှမ်းပြည်နယ် နှင့် ချင်းပြည်နယ်အစရှိသော
+        ` ခုနှစ်အထိ မန္တလေးတိုင်း၊မကွေးတိုငး၊ရှမ်းပြည်နယ် နှင့် ချင်းပြည်နယ်အစရှိသော
         ပြည်နယ်နှင့်တိုင်းအသီးသီးတွင် စီမံကိန်း`,
-
-        line4: `(၁၀၀)ကျော်ခန့် ပြီးမြောက်လွှဲအပ်နိုင်ခဲ့ပြီဖြစ်ပါသည်။`,
-        line5: `       ကုန်ထုပ်လုပ်မှု Manufacturing Company ကိုမှု ၂၀၁၇ နိုဝင်ဘာတွင် တည်ထောင်ခဲ့ပြီး`,
-        line6: `4 Angle, C-Purlin ,Deck-Sheet အစရှိသည့်သံပြားပုံဖော်ခြင်း (roll-farming) အားစတင်လုပ်ဆောင်ခဲ့ပါသည်။`,
-        line7: ` တစ်လထုတ်လုပ်မှုစွမ်းရည်မှာ 1500-2000 MT ဖြစ်ပြီး ယခုအခါ မြန်မာနိုင်ငံအထက်ပိုင်းတွင်
+        `(၁၀၀)ကျော်ခန့် ပြီးမြောက်လွှဲအပ်နိုင်ခဲ့ပြီဖြစ်ပါသည်။`,
+        `       ကုန်ထုပ်လုပ်မှု Manufacturing Company ကိုမှု ၂၀၁၇ နိုဝင်ဘာတွင် တည်ထောင်ခဲ့ပြီး`,
+        `4 Angle, C-Purlin ,Deck-Sheet အစရှိသည့်သံပြားပုံဖော်ခြင်း (roll-farming) အားစတင်လုပ်ဆောင်ခဲ့ပါသည်။`,
+        ` တစ်လထုတ်လုပ်မှုစွမ်းရည်မှာ 1500-2000 MT ဖြစ်ပြီး ယခုအခါ မြန်မာနိုင်ငံအထက်ပိုင်းတွင်
         ဖြန့်ဖြူးရောင်းချလျှက်ရှိသည်။`,
-
-      },
-      values: {
-        people: 'အဖွဲ့အစည်း',
-        teamwork: 'အသင်းအဖွဲ့လိုက်လုပ်ကိုင်မှု',
-        trust: 'ယုံကြည်မှု',
-        integrity: 'သမာဓိရှိမှု',
-        innovation: 'ဆန်းသစ်တီထွင်မှု',
-        sustainability: 'ရည်ရှည်တည်တံ့ခိုင်မြဲမှုအဖွဲ့အစည်း'
-      },
-      services: {
-        rcc: 'RC ဆောက်လုပ်ရေး',
-        roadWay: 'လမ်း၊တံတား ဆောက်လုပ်ရေး',
-        steelStructure: 'Steel structure ဆောက်လုပ်ရေး',
-        steelFabriction: 'Steel structure ထုတ်လုပ်၊တပ်ဆင်ရေး',
-        ppgiAndGiRollForming: 'သွပ်ပြား၊စီချန်နယ်၊Deck Sheet ထုတ်လုပ်ဖြန့်ချီရေး',
-        machineryRentalAndEarthworks: 'စက်၊ယန္တရားငှားယမ်းခြင်းနှင့် မြေသားလုပ်ငန်း'
-      }
+      ],
+      values: 'တန်ဖိုးများ',
+      valuesContent: ['အဖွဲ့အစည်း','အသင်းအဖွဲ့လိုက်လုပ်ကိုင်မှု','ယုံကြည်မှု','သမာဓိရှိမှု','ဆန်းသစ်တီထွင်မှု','ရည်ရှည်တည်တံ့ခိုင်မြဲမှုအဖွဲ့အစည်း'],
+      services: 'ဝန်ဆောင်မှုများ',
+      servicesContent: ['RC ဆောက်လုပ်ရေး','လမ်း၊တံတား ဆောက်လုပ်ရေး','Steel structure ဆောက်လုပ်ရေး','Steel structure ထုတ်လုပ်၊တပ်ဆင်ရေး','သွပ်ပြား၊စီချန်နယ်၊Deck Sheet ထုတ်လုပ်ဖြန့်ချီရေး','စက်၊ယန္တရားငှားယမ်းခြင်းနှင့် မြေသားလုပ်ငန်း']
     },
     english: {
       vision: 'vision',
       visionContent: 'To be the supreme lead in Myanmar’s RC and Steel Structure construction projects, in the future.',
-      ourMission: {
-        line1: 'Committed to deliver the best solution to our customers through our services and products. ',
-        line2: 'Building long-term partnership with our customers, supplier, team while creating sustainable future.'
+      mission: 'Our Mission',
+      missionContent: [
+        'Committed to deliver the best solution to our customers through our services and products.',
+        'Building long-term partnership with our customers, supplier, team while creating sustainable future.'
+      ],
+      ourStory: 'Our Story',
+      ourStoryContent: [
+        `Founded By U Sanni in February 2010, in Mandalay.`,
+        `Partnership with government’s`,
+        ` construction projects and till now 2022 in Mandalay,Magway,Shan state and Chin states.`,
+        ` Founded : February 2010`,
+        ` Manufacturing`,
+        ` Established : November 2017`,
+        ` Roll Forming : 4-Angle Roofing`,
+        ` C- Purlin and Deck Sheet`,
+        ` Capacity :2000MT/month`,
+      ],
       },
-      ourStory: {
-        line1: `Founded By U Sanni in February 2010, in Mandalay.`,
-        line2: ` Partnership with government’s`,
-        line3: ` construction projects and till now 2022 in Mandalay,Magway,Shan state and Chin states.`,
-        line4: `Founded : February 2010`,
-        line5: `Manufacturing`,
-        line6: `Established : November 2017`,
-        line7: `  Roll Forming : 4-Angle Roofing`,
-        line8: ` C- Purlin and Deck Sheet`,
-        line9: ` Capacity :2000MT/month`,
-      },
-      values: {
-        people: 'people',
-        teamwork: 'teamwork',
-        trust: 'trust',
-        integrity: 'integrity',
-        innovation: 'innovation',
-        sustainability: 'sustainability'
-      },
-      services: {
-        rcc: 'R.C.C Construction',
-        roadWay: 'Roadway Construction',
-        steelStructure: 'Steel Structure Construction',
-        steelFabriction: 'Steel Fabrication',
-        ppgiAndGiRollForming: 'PPGI/GL and GI Roll-Forming',
-        machineryRentalAndEarthworks: 'Machinery Rental and Earthworks'
-      }
-    }
+      values: 'Values',
+      valuesContent: ['people','teamwork','trust','integrity','innovation','sustainability'],
+      services: 'services',
+      servicesContent: [
+        'R.C.C Construction',
+        'Roadway Construction',
+        'Steel Structure Construction',
+        'Steel Fabrication',
+        'Steel Fabrication',
+        'Machinery Rental and Earthworks'
+      ]
   }
 }
 
+function changeLanguage(language) {
+  const navbar = document.getElementsByClassName('nav-link');
+  const vision = document.getElementsByClassName('vision');
+  const visionContent = document.getElementsByClassName('vision-content');
+  const mission = document.getElementsByClassName('mission');
+  const missionContent = document.getElementsByClassName('mission-content');
+  const ourStory = document.getElementsByClassName('our-story');
+  const ourStoryContent = document.getElementsByClassName('our-story-content');
+  const values = document.getElementsByClassName('values');
+  const valueContent = document.getElementsByClassName('value');
+  const services = document.getElementsByClassName('services');
+  const serviceContent = document.getElementsByClassName('service');
+
+  if(language === 'myanmar') {
+    for (let index = 0; index < navbar.length; index++) {
+      navbar[index].textContent = data.menu.myanmar[index];
+    }
+    vision[0].textContent = data.content.myanmar.vision;
+    visionContent[0].textContent = data.content.myanmar.visionContent;
+    mission[0].textContent = data.content.myanmar.mission;
+    for (let index = 0; index < missionContent.length; index++) {
+      missionContent[index].textContent = data.content.myanmar.missionContent[index];
+    }
+    ourStory[0].textContent = data.content.myanmar.ourStory;
+    for (let index = 0; index < ourStoryContent.length; index++) {
+      ourStoryContent[index].textContent = data.content.myanmar.ourStoryContent[index];
+    }
+    values[0].textContent = data.content.myanmar.values; 
+    for (let index = 0; index < valueContent.length; index++) {
+      valueContent[index].textContent = data.content.myanmar.valuesContent[index];
+    }
+    services[0].textContent = data.content.myanmar.services; 
+    for (let index = 0; index < valueContent.length; index++) {
+      serviceContent[index].textContent = data.content.myanmar.servicesContent[index];
+    }
+  } 
+  if(language === 'english')  {
+    for (let index = 0; index < navbar.length; index++) {
+      navbar[index].textContent = data.menu.english[index];
+    }
+    vision[0].textContent = data.content.english.vision;
+    visionContent[0].textContent = data.content.english.visionContent;
+    mission[0].textContent = data.content.english.mission;
+    for (let index = 0; index < missionContent.length; index++) {
+      missionContent[index].textContent = data.content.english.missionContent[index];
+    }
+    ourStory[0].textContent = data.content.english.ourStory;
+    for (let index = 0; index < ourStoryContent.length; index++) {
+      ourStoryContent[index].textContent = data.content.english.ourStoryContent[index];
+    }
+    values[0].textContent = data.content.english.values; 
+    for (let index = 0; index < valueContent.length; index++) {
+      valueContent[index].textContent = data.content.english.valuesContent[index];
+    }
+    services[0].textContent = data.content.english.services; 
+    for (let index = 0; index < valueContent.length; index++) {
+      serviceContent[index].textContent = data.content.english.servicesContent[index];
+    }
+  }
+}
